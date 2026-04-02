4c7d936c7b95e5c1063b0615610a783a171fb066
heakvn <jiakun90@qq.com>
2026-04-02 14:54:46 +0800

    Update footer.tsx

--- src/components/frontend/shared/footer.tsx
+++ src/components/frontend/shared/footer.tsx
@@ -5,22 +5,14 @@ export function Footer() {
   return (
     <footer className="container py-4 text-center text-sm text-muted-foreground mt-10">
       <div className="my-3 flex items-center justify-center gap-3">
-        <b>其他项目: </b>
-        <a href="https://www.redirectchecker.org/" className="text-primary flex items-center">
-          <img alt="Redirect Checker" src="https://www.faviconextractor.com/favicon/www.redirectchecker.org" className="h-5 w-5 mr-1 rounded-full bg-secondary" width={20} height={20} />
-          <span>Redirect Checker</span>
-        </a>
-        <a href="https://www.fontgenerator.dev/" className="text-primary flex items-center">
-          <img alt="Font Generator" src="https://www.faviconextractor.com/favicon/www.fontgenerator.dev" className="h-5 w-5 mr-1 rounded-full bg-secondary" width={20} height={20} />
-          <span>Font Generator</span>
-        </a>
-        <a href="https://www.favicongenerator.io/" className="text-primary flex items-center">
-          <img alt="Favicon Generator" src="https://www.faviconextractor.com/favicon/www.favicongenerator.io" className="h-5 w-5 mr-1 rounded-full bg-secondary" width={20} height={20} />
-          <span>Favicon Generator</span>
+        <b>DigitalPlat: </b>
+        <a href="https://dash.domain.digitalplat.org/signup?ref=DP0pecdU5m/" className="text-primary flex items-center">
+          <img alt="DigitalPlat FreeDomain" src="https://www.faviconextractor.com/favicon/digitalplat.org" className="h-5 w-5 mr-1 rounded-full bg-secondary" width={20} height={20} />
+          <span>This Website is Powered by DigitalPlat FreeDomain,Get a free domain from DigitalPlat</span>  
         </a>
       </div>
       <span>
-        © {new Date().getFullYear()} <a href="https://getfavicon.jiakun.us.kg">{appConfig.appName}</a>. All rights reserved.
+        © {new Date().getFullYear()} <a href="https://favicon.qvv.qzz.io">{appConfig.appName}</a>. All rights reserved.
       </span>
     </footer>
   );
